715f8fd662442700e3bab1dcaf5b62e4916dcb78
mtinti <michele.tinti@gmail.com>
2023-04-17 17:39:46 +0100

    start

---
+++ config.js
@@ -0,0 +1,29 @@
+let X1='SignedRank';
+let Y1='FDR';
+let X2='log10iBAQ';
+let Y2='SignedRank';
+
+let FLIP_Y1=true;
+let FLIP_Y2=false;
+
+let PLOT_WIDTH=550;
+let PLOT_HEIGHT=400;
+
+let PROJECT_TITLE='Cryptosporidium  Oocyst Proteome';
+
+let margin = 0.05;
+
+let BOX_PLOT_COLS_1 = ['iBAQ SPORO_1','iBAQ SPORO_2','iBAQ SPORO_3'];
+let BOX_PLOT_COLS_2 = ['iBAQ WALL_1','iBAQ WALL_2','iBAQ WALL_3'];
+
+let CONDITION_1 = 'Sporo'
+let CONDITION_2 = 'Wall'
+
+
+let COLOR_SELECTION_TABLE = 'orange';
+let COLOR_SELECTION_CIRCLE = 'orange';
+
+let Y_LABEL_BARPLOT = 'iBAQ Intensity';
+
+let File_1 = 'indata.csv';
+let File_2 = 'indata2.csv';
